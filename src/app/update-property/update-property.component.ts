54ffc04d9dbbabcd542c19d9e5be1ad51309b0f6
Alexandros aristeridis@hotmail.gr <aristeridis@hotmail.gr>
2024-10-01 20:34:48 +0300

    install files for create & update

---
+++ src/app/update-property/update-property.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-property',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-property.component.html',
+  styleUrl: './update-property.component.css'
+})
+export class UpdatePropertyComponent {
+
+}
